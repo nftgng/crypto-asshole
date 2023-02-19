aacbdfc9141a46166c799c0e2cc60e3dff5b5860
nftgng <124895152+nftgng@users.noreply.github.com>
2023-02-19 02:01:37 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# change to eth-asshole directory
+cd ~/eth-asshole
+
+# install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install required Node.js packages
+npm install ethers
+npm install nodemailer
+npm install dotenv
+
+# make script executable
+chmod +x script.js
+
+# run the script
+./script.js
